ac62ffa2882c3ff537f35d14e8a841c1f1c4c59b
PayNoahttention <NSolis1993@gmail.com>
2020-03-06 18:35:47 -0600

    This was supposed to show buttons. It doesn't but I wanna show progress.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,31 @@
 
 $(document).ready(function() {
-    var poke = ['Totodile', 'Lapras', 'Gyarados', 'Squirtle', 'Psyduck', 'Suicune', 'Frokie', 'Manaphy', 'Kyogre','Mudkip'];
-});
 
-$('#pokemon-api').empty();
-      var topic = $(this).attr('data-name');
-      var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + topic + '&GrmPeN2RHRwvZtfl4S3EEFwr8KxbSoum';
+    // Water pokemon are best pokemon
+    var poke = ['Totodile', 'Lapras', 'Gyarados', 'Squirtle', 'Psyduck', 'Suicune', 'Frokie', 'Manaphy', 'Kyogre','Mudkip'];
+
+    function displayInfo(){
+        $('#pokemon-api').empty();
+            var topic = $(this).attr('data-name');
+            var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + topic + '&GrmPeN2RHRwvZtfl4S3EEFwr8KxbSoum';
+    
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+    
+            // show me those buttttttoooonnnsss
+    
+            function showButtons() {
+                $('.buttons').empty();
+                for (var i = 0; i < topics.length; i++) {
+                var makeButtons = $('<button>');
+                makeButtons.addClass('topic btn btn-info');
+                makeButtons.attr('data-name', poke[i]);
+                makeButtons.text(poke[i]);
+                $('.buttons').append(makeButtons);
+                }
+            }
+        }
+});
+showButtons();
